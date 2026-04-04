5a46ac297a4657ade291470c79b52c6c6678d9af
dinosaurwastaken <mhatre16pratham@gmail.com>
2026-04-04 16:05:14 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int>ret;
+        for(int i = 0 ; i < nums.size() ; i++ ){
+            if(nums[i] != val){
+                ret.push_back(nums[i]);
+            }
+        }
+        nums = ret;
+        return ret.size();
+    }
+};
