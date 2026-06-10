d0b0c3f723c1a83315bb888a9c1e098ce68d43a5
dinosaurwastaken <mhatre16pratham@gmail.com>
2026-06-10 20:03:33 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int maxi = *max_element(nums.begin(),nums.end());
+        //int mini = *min_element(nums.begin(),nums.end());
+        //if(mini < 0) mini = 1;
+        unordered_set<int>data;
+        for(int i = 0 ; i < nums.size() ; i++){
+            data.insert(nums[i]);
+        }
+        for(int i = 1 ; i <= maxi ; i++){
+            if(data.count(i) == 0) return i;
+        }
+    }
+};
