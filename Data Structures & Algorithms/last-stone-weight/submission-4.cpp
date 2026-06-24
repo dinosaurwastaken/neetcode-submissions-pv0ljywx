bdbff00b94b8a74a8df0cf19836850fbdd475bb6
dinosaurwastaken <mhatre16pratham@gmail.com>
2026-06-24 22:25:38 +0530

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>stone;
+        for(auto it : stones){
+            stone.push(it);
+        }
+        while(stone.size() > 1){
+            int st1 = stone.top();
+            stone.pop();
+            int st2 = stone.top();
+            stone.pop();
+            if(st1 == st2){
+                continue;
+            }
+            else if(st1 > st2){
+                stone.push(st1-st2);
+                
+            }
+            else{
+                stone.push(st2-st1);
+            }
+        }
+        if(stone.empty()) 
+            return 0;
+        return stone.top();
+    }
+};
