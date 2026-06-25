96d7f5a21c2db2fa316db064feb80a4928a257ce
dinosaurwastaken <mhatre16pratham@gmail.com>
2026-06-25 12:30:17 +0530

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* mover = head;
+        Node* copy = new Node(0);
+        Node* deepmover = copy;
+        while(mover){
+            Node*temp = new Node(mover->val);
+            deepmover->next = temp;
+            deepmover = temp;
+            mover = mover->next;
+        }
+        mover = head;
+        deepmover = copy->next;
+        Node* mover1 = head;
+        Node* mover2 = copy->next;
+        while(mover != nullptr){
+            Node* rand = mover->random;
+            Node* temp = deepmover;
+            if(rand == nullptr){
+                temp->random = nullptr;
+            }
+            else{
+                while(mover1 != rand){
+                    mover1 = mover1->next;
+                    mover2 = mover2->next;
+                }
+                temp->random = mover2;
+                mover1 = head;
+                mover2 = copy->next;
+            }
+            mover = mover->next;
+            deepmover = deepmover->next;
+        }
+        return copy->next;
+    }
+};
